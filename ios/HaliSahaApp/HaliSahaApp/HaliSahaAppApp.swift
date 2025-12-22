d197ad0f49151fb09be138486e13e3e12c8cd153
Mehmet Mert Mazıcı <mazicimert35@gmail.com>
2025-12-22 14:09:00 +0300

    Proje yapısı kuruldu: iOS ve Android klasörleri eklendi.

---
+++ ios/HaliSahaApp/HaliSahaApp/HaliSahaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaliSahaAppApp.swift
+//  HaliSahaApp
+//
+//  Created by Mehmet Mert Mazıcı on 22.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct HaliSahaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
